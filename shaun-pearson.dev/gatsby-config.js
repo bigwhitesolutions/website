464a18e4cc02817357dd5afedb069d2bb588ff23
shaunpearsondev <90578327+shaunpearsondev@users.noreply.github.com>
2023-03-13 21:38:49 +0000

    Update site metadata

--- shaun-pearson.dev/gatsby-config.js
+++ shaun-pearson.dev/gatsby-config.js
@@ -8,6 +8,13 @@ module.exports = {
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
     siteTitleAlt: `bigwhitesolutions.com`,
+    siteHeadline: `Big White Solutions`,
+    siteUrl: `https://bigwhitesolutions.com`,
+    siteTitle: `Big White Solutions`,
+    author: '@shaunpearsondev',
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    siteDescription: `Company Website`,
   },
   plugins: [
     {
